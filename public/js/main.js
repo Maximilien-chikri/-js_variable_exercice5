4e76e2264b8e1424304c8cf7cc29f380f562df17
Maximilien <maximilienchikri@gmail.com>
2020-12-29 09:45:57 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+// exercice 1
+nbr1 = 4;
+nbr2 = 2;
+console.log(`${nbr1} + ${nbr2} = ${nbr1 + nbr2}`);
+console.log(`${nbr1} - ${nbr2} = ${nbr1 - nbr2}`);
+console.log(`${nbr1} * ${nbr2} = ${nbr1 * nbr2}`);
+console.log(`${nbr1} / ${nbr2} = ${nbr1 / nbr2}`);
+
+// exercice 2
+
+let age = parseInt(prompt("votre âge"));
+let prenom = prompt("votre prénom");
+console.log(`${prenom} a ${age} ans cette année`);
+console.log(`${age + 3}`);
+
+
